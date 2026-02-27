c5fcb2220a74691b073208adc0510fc4676f4f56
Joshua Mendez <joshuagmendez14@gmail.com>
2026-02-27 09:02:30 -0600

    Metodo Mkdir anidadido

--- src/ComandLogic.java
+++ src/ComandLogic.java
@@ -1,3 +1,6 @@
+
+import java.io.File;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,4 +13,25 @@
  */
 public class ComandLogic {
     
+    public String cmdMkdir(String nombre){
+        if(nombre.isEmpty())
+            return "Uso: MKdir <nombre>";
+        File dir=new File(ruta, nombre);
+        if(dir.mkdir())
+            return "Carpeta "+nombre+" creada exitosamente.";
+        
+        return "No se pudo crear carpeta: "+nombre;
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+ 
+   
 }
